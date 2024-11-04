6e3267366ead471b8aa222a33dfd3a0914a63bb4
Yousef <theyousefkhan@gmail.com>
2024-11-04 06:15:29 -0800

    too many, removed preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { Sugar } from "react-preloaders";
+import React from "react";
 
 import Navbar from "./components/navbar/navbar";
 import Hero from "./components/hero/hero";
@@ -7,22 +6,11 @@ import Footer from "./components/footer/footer";
 import "./App.css";
 
 function App() {
-
-  // Safari Support
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      document.getElementById("preloader").classList.add("hide");
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <React.Fragment>
       <Navbar />
       <Hero />
       <Footer />
-      <Sugar background="oklch(var(--n))" color="oklch(var(--b1))" time={0}/>
     </React.Fragment>
   );
 }
