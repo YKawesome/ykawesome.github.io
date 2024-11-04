cffe1eeca3770567207866bdbade1304e919349c
Yousef <theyousefkhan@gmail.com>
2024-11-04 00:03:40 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import Footer from "./components/footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
