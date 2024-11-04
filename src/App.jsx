85e3e7be683e67a3ab8439a2d32cdb49571bf37f
Yousef <theyousefkhan@gmail.com>
2024-11-04 05:27:36 -0800

    preloader safari bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Sugar } from "react-preloaders";
 
 import Navbar from "./components/navbar/navbar";
@@ -7,6 +7,16 @@ import Footer from "./components/footer/footer";
 import "./App.css";
 
 function App() {
+
+  // Safari Support
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      document.getElementById("preloader").classList.add("hide");
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <React.Fragment>
       <Navbar />
