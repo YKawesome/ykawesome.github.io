4fb31b6da6c00577dd20b187a7cad4523b2617b3
Yousef <theyousefkhan@gmail.com>
2024-11-04 05:10:22 -0800

    animatons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import Navbar from "./components/navbar";
-import Hero from "./components/hero";
-import Footer from "./components/footer";
+import React from "react";
+import { Sugar } from "react-preloaders";
+
+import Navbar from "./components/navbar/navbar";
+import Hero from "./components/hero/hero";
+import Footer from "./components/footer/footer";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Navbar />
       <Hero />
       <Footer />
-    </>
+      <Sugar background="oklch(var(--n))" color="oklch(var(--b1))" time={1000}/>
+    </React.Fragment>
   );
 }
 
